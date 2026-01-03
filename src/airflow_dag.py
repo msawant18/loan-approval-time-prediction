dfa1e102083f445478df12592f47dd2f0e3c97e8
Mrunal Sawant <111610357+msawant18@users.noreply.github.com>
2026-01-02 18:58:37 -0700

    Update airflow_dag.py

--- src/airflow_dag.py
+++ src/airflow_dag.py
@@ -1,1 +1,17 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
 
+def run_pipeline():
+    print("Pipeline executed")
+
+with DAG(
+    "loan_approval_time_pipeline",
+    start_date=datetime(2024, 1, 1),
+    schedule="@weekly",
+    catchup=False,
+) as dag:
+    run = PythonOperator(
+        task_id="run_pipeline",
+        python_callable=run_pipeline
+    )
