ee6f958bd6e69b56b35b8e54d7f2a8f98981f9ab
Mrunal Sawant <111610357+msawant18@users.noreply.github.com>
2026-01-02 18:57:18 -0700

    Update ge_validation.py

--- src/ge_validation.py
+++ src/ge_validation.py
@@ -1,1 +1,18 @@
+import great_expectations as ge
+
+def validate_data(df):
+    gdf = ge.from_pandas(df)
+
+    gdf.expect_column_values_to_be_between("holiday_count", 0, 30)
+    gdf.expect_column_values_to_be_between("branch_load", 1, 20000)
+
+    gdf.expect_column_values_to_be_between(
+        "days_to_approval", 0, 180,
+        row_condition="days_to_approval.notnull()",
+        condition_parser="pandas"
+    )
+
+    result = gdf.validate()
+    assert result["success"]
+    return result
 
