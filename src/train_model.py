9c8406c55fa94b474770337b22ca357758d25d99
Mrunal Sawant <111610357+msawant18@users.noreply.github.com>
2026-01-02 18:58:07 -0700

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,20 @@
+import mlflow
+import lightgbm as lgb
+
+def train(df):
+    X = df[["holiday_count", "branch_load"]]
+    y = df["days_to_approval"]
+
+    with mlflow.start_run():
+        model = lgb.LGBMRegressor(
+            n_estimators=300,
+            learning_rate=0.05
+        )
+        model.fit(X, y)
+
+        preds = model.predict(X)
+        mae = abs(preds - y).mean()
+
+        mlflow.log_metric("mae", mae)
+        mlflow.sklearn.log_model(model, "model")
 
